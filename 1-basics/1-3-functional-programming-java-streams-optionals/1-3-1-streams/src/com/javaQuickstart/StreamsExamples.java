dfe0a40c9996b95c61958c827be116a975877148
Oscar Barlow <oscar.barlow@gmail.com>
2017-11-03 02:28:23 +0000

    friendlier commenting out

--- 1-basics/1-3-functional-programming-java-streams-optionals/1-3-1-streams/src/com/javaQuickstart/StreamsExamples.java
+++ 1-basics/1-3-functional-programming-java-streams-optionals/1-3-1-streams/src/com/javaQuickstart/StreamsExamples.java
@@ -20,30 +20,30 @@ public class StreamsExamples {
     public static void main(String[] args) {
       // Comment and uncomment the sections of code below so you can see which bit does what
 
-//      int[] simpleIntegers = new int[] {34, 99, 105, 1};
-//      int simpleTotal = integerSummer.sum(simpleIntegers);
-//      System.out.println("The sum is: " + simpleTotal);
-//
+      int[] simpleIntegers = new int[] {34, 99, 105, 1};
+      int simpleTotal = integerSummer.sum(simpleIntegers);
+      System.out.println("The sum is: " + simpleTotal);
+
 //      List<Integer> integerArrayList = new ArrayList<>(asList(64, 876, 99, 100));
 //      int moreComplicatedTotal = integerSummer.sum(integerArrayList);
 //      System.out.println("The sum is: " + moreComplicatedTotal);
-//
+
 //      FireAndForgetRequest fireAndForgetRequest1 = new FireAndForgetRequest(
 //          "*** some important data 123 ***");
 //      FireAndForgetRequest fireAndForgetRequest2 = new FireAndForgetRequest(
 //          "*** more important data 456 ***");
 //      List<FireAndForgetRequest> requests = Arrays.asList(fireAndForgetRequest1, fireAndForgetRequest2);
 //      fireAndForgetter.sendRequests(requests);
-//
+
 //      List<String> names = asList("Bob", "Yuki", "Manuela", "Tamara");
 //      nameFilterer.shortNamesBeginningWithB(names);
 //      nameFilterer.onlyJapaneseNames(names);
 
-      List<String> cityNames = asList("London", "Los Angeles", "Paris");
-      List<City> cities = cityNames.stream()
-          .map(cityTransformer)
-          .collect(Collectors.toList());
-      cities.forEach(c -> System.out.println(c.toString()));
+//      List<String> cityNames = asList("London", "Los Angeles", "Paris");
+//      List<City> cities = cityNames.stream()
+//          .map(cityTransformer)
+//          .collect(Collectors.toList());
+//      cities.forEach(c -> System.out.println(c.toString()));
     }
 
 
